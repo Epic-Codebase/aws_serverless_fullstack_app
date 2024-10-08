5db0114cb8ad177c9a46362e26928c45e9a70c94
Lucas Belo <lmbelo@Lucass-MacBook-Pro.local>
2024-10-08 08:02:11 -0300

    Deploying UI

--- services/tasks_ui/src/aws-exports.js
+++ services/tasks_ui/src/aws-exports.js
@@ -1,9 +1,9 @@
 export default {
     Auth: {
-        Cognito: {
+
             region: 'eu-west-1',
             userPoolId: 'eu-west-1_nxEjJL2GE',
             userPoolWebClientId: '52dti3caarriqbfhsn3169o3ma'
-        }
+
     }
   };
